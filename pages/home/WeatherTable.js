5282a01c3f76fa2ce613f3cdc0230e4c757ff31c
C. Alex Wensley <cwensley@gmail.com>
2019-10-21 18:06:09 -0600

    changed CSS and layout architecture

---
+++ pages/home/WeatherTable.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+class WeatherTable extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: false,
+            input: '',
+            zipCode: 'N/A',
+            currentWeather: {
+                main: {
+                    temp: '255.38',
+                    humidity: '',
+                    temp_min: '255.38',
+                    temp_max: '255.38',
+                },
+                name: '',
+                wind: {
+                    speed: '',
+                },
+                weather: [{
+                    description: '',
+                }],
+            },
+        }
+    }
+
+    onInputChange=(event) => {this.setState({input: event.target.value})};
+
+    onButtonSubmit = () => {
+        this.setState({isLoading: true});
+        this.setState({zipCode: this.state.input});
+        fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.input},us&APPID=7f838af83bf39591f00de86b6f7329e1`)
+            .then(response=> response.json())
+            .then(data => this.setState({ currentWeather: data,isLoading: false}))
+            .catch(err => {window.location.reload(true)});
+    };
+
+    render() {
+        const {currentWeather,zipCode,isLoading} = this.state;
+
+        if (isLoading) {return <p>Loading...</p>};
+
+        var windSpeed = (currentWeather.wind.speed)*2.237;
+        var highTemp = (currentWeather.main.temp_max-273.15)*9/5+32;
+        var currentTemp = (currentWeather.main.temp-273.15)*9/5+32;
+        var lowTemp = (currentWeather.main.temp_min-273.15)*9/5+32;
+
+        return (
+            <div>
+                <table className="table table-dark table-striped table-bordered table-sm w-75 m-auto">
+                    <tbody>
+                    <tr className="h6">
+                        <td className="align-middle text-warning">Enter a Zip Code for current Weather data:</td>
+                        <td>
+                            <input type="text" size="8" maxLength="5" className="h6" onChange={this.onInputChange}/>
+                            <button type="submit" value="Submit" className="h6" onClick={this.onButtonSubmit}>Submit</button>
+                        </td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Current Temp.:</td>
+                        <td>{currentTemp.toFixed(1)} F</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>High Temp.:</td>
+                        <td>{highTemp.toFixed(1)} F</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Low Temp.:</td>
+                        <td>{lowTemp.toFixed(1)} F</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Cloudiness:</td>
+                        <td>{currentWeather.weather[0].description}</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Wind Speed:</td>
+                        <td>{windSpeed.toFixed(1)} mph</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Humidity:</td>
+                        <td>{currentWeather.main.humidity}%</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>City:</td>
+                        <td>{currentWeather.name}</td>
+                    </tr>
+                    <tr className="h6">
+                        <td>Zipcode:</td>
+                        <td>{zipCode}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default WeatherTable;
+
