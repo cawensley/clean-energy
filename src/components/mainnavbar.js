bf74253f7d9a16455943e15c0575428e999ff0c1
C. Alex Wensley <cwensley@gmail.com>
2019-10-21 18:13:31 -0600

    updated navbar

--- src/components/mainnavbar.js
+++ src/components/mainnavbar.js
@@ -3,20 +3,22 @@ import React from "react";
 
 const MainNavBar=()=> {
     return (
-        <nav className="navbar navbar-expand bg-secondary fixed-top">
-                <ul className="navbar-nav">
-                    <li className="nav-item mx-1">
-                        <Link to="/" className="nav-link py-3 px-3 rounded text-light">Home</Link>
-                    </li>
-                    <li className="nav-item mx-1">
-                        <Link to="/education" className="nav-link py-3 px-3 rounded text-light">Education</Link>
-                    </li>
-                    <li className="nav-item mx-1">
-                        <Link to="/about" className="nav-link py-3 px-3 rounded text-light">About</Link>
-                    </li>
-                </ul>
+        <nav className="navbar navbar-expand bg-secondary fixed-top text-uppercase" id="mainNav">
+            <div className="container">
+                <Link to="/" className="navbar-brand nav-link py-3 px-3 rounded">Home</Link>
+                <div>
+                    <ul className="navbar-nav navbar-expand ml-auto">
+                        <li className="nav-item mx-1">
+                            <Link to="/education" className="nav-link py-3 px-3 rounded">Education</Link>
+                        </li>
+                        <li className="nav-item mx-1">
+                            <Link to="/about" className="nav-link py-3 px-3 rounded">About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
-)
+    )
 };
 
 export default MainNavBar;
