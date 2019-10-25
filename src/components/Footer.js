2578bcce0ccf10a66c9dd7bc3b86345f6377ca56
C. Alex Wensley <cwensley@gmail.com>
2019-10-25 11:39:37 -0600

    removed two columns, merged to one

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,42 +5,33 @@ const Footer=()=> {
         <div>
         <footer className="footer text-center">
             <div className="container">
-                <div className="row">
 
                     {/*Footer Location*/}
-                    <div className="col-lg-6 mb-5 mb-lg-0">
-                        <h4 className="text-uppercase mb-4">WRA Headquarters Location</h4>
-                        <p className="lead mb-0">2260 Baseline Road, Suite 200</p>
-                        <p className="lead mb-0">Boulder CO 80302</p>
-                        <p className="lead mb-0">Phone: 303.444.1188</p>
-                        <p className="lead mb-0">Fax: 303.786.8054</p>
-                    </div>
+                    <div className="mb-3 mb-lg-0">
+                        <h4 className="text-uppercase lead font-weight-bold mb-3">WRA Headquarters</h4>
+                        <p className="copyright bg-secondary mb-0">2260 Baseline Road, Suite 200</p>
+                        <p className="copyright bg-secondary  mb-0">Boulder CO 80302</p>
+                        <p className="copyright bg-secondary  mb-0">Phone: 303.444.1188</p>
+                        <p className="copyright bg-secondary  mb-1">Fax: 303.786.8054</p>
 
-                    {/*Footer Social Icons*/}
-                    <div className="col-lg-6 mb-5 mb-lg-0">
-                        <h4 className="text-uppercase mb-4">WRA Around the Web</h4>
+                        {/*Footer Social Icons*/}
                         <a className="btn btn-outline-light btn-social mx-1"
                            href="https://www.facebook.com/westernresourceadvocates">
-                            <i className="fab fa-fw fa-facebook-f"></i>
-                        </a>
+                            <i className="fab fa-fw fa-facebook-f"></i></a>
                         <a className="btn btn-outline-light btn-social mx-1" href="https://twitter.com/wradv">
-                            <i className="fab fa-fw fa-twitter"></i>
-                        </a>
+                            <i className="fab fa-fw fa-twitter"></i></a>
                         <a className="btn btn-outline-light btn-social mx-1"
                            href="https://www.linkedin.com/company/western-resource-advocates">
-                            <i className="fab fa-fw fa-linkedin-in"></i>
-                        </a>
+                            <i className="fab fa-fw fa-linkedin-in"></i></a>
                         <a className="btn btn-outline-light btn-social mx-1"
                            href="https://www.instagram.com/westernresourceadvocates/">
-                            <i className="fab fa-instagram"></i>
-                        </a>
+                            <i className="fab fa-instagram"></i></a>
                     </div>
-                </div>
             </div>
         </footer>
 
                 {/*Copyright Section*/}
-            <section className="copyright py-4 text-center text-white">
+            <section className="copyright py-1 text-center text-white">
             <div className="container">
                 <small>Copyright &copy; Clean-Energy Website 2019</small>
             </div>
