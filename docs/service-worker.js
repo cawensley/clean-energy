dad75ef4538a4980b6ad033bbce8716e07a39d46
C. Alex Wensley <cwensley@gmail.com>
2019-10-25 11:41:47 -0600

    second publish with homepage

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cleanNRGtesting/precache-manifest.9e6f0b0a53ebbdccb9c4d374560dc162.js"
+  "/clean-energy/precache-manifest.4b5847fdf74cb90a23783acdda4119ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cleanNRGtesting/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clean-energy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
