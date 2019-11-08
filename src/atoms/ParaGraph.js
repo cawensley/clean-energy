b1ea67506a343904b1362f1cce478635d10362a9
Meredith Wilson <cwensley@gmail.com>
2019-11-08 03:12:03 -0700

    deleted files to be consistent with ABEM styling

--- src/atoms/ParaGraph.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ParaGraph=({text})=> {
-    return (
-        <div className="lead text-white mb-4 transparent p-1">{text}</div>
-    )
-};
-
-export default ParaGraph;
