ae8d1051766f4dc9070d9eb7bb2d6b0933746561
Meredith Wilson <cwensley@gmail.com>
2019-11-08 16:21:29 -0700

    second deployment

--- src/atoms/ParaGraphtext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ParaGraphtext=({text})=> {
-    return (
-        <div className="lead text-white mb-4 p-1 a-ParaGraph_background-transparent">{text}</div>
-    )
-};
-
-export default ParaGraphtext;
--- src/atoms/tempconversion.js
+++
@@ -1,7 +0,0 @@
-const Fahrenheit=({Ktemp})=> {
-    var Ftemp = (Ktemp-273.15)*9/5+32;
-    var Fsigfigs = Ftemp.toFixed(1);
-    return (Fsigfigs)
-};
-
-export default Fahrenheit;
